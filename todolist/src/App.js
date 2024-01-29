4250f36307bd792ca35f6b156c529dfae46f93d6
Travoori <tharuniravoori1999@gmail.com>
2024-01-29 14:28:23 -0600

    done
    tododone

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,9 +1,32 @@
-import "./App.css";
-import Taskform from "./taskform";
+import React, { useState } from "react";
 function App() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+  const addtodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+    setList([...list, newTodo]);
+    setInput("");
+  };
   return (
     <div>
-      <Taskform />
+      <h1>Todolist!</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => addtodo(input)}>Add</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button>&times;</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- todolist/src/taskform.js
+++
@@ -1,9 +0,0 @@
-function Taskform() {
-  return (
-    <form>
-      <button>+</button>
-      <input type="text" placeholder="Add your next task" />
-    </form>
-  );
-}
-export default Taskform;
